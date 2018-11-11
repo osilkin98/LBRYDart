58233a1f104461d0f275d90757d72b7fe996e069
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 17:47:12 -0600

    Adds docstring

--- lib/src/lbrycrd_api.dart
+++ lib/src/lbrycrd_api.dart
@@ -17,6 +17,13 @@ class LbrycrdApi extends LbryBaseApi {
   LbrycrdApi.credentials(String username, String password,
       {this.timeout = 600}) : _basicAuth = [username, password];
 
+  /// Makes a call to the LBRYCRD API
+  ///
+  /// Invokes a call to the [method] function with parameters
+  /// [params] in the LBRYCRD API, and the username and
+  /// password pair [_basicAuth] to authenticate. The client
+  /// waits for [timeout] seconds before giving up.
+  /// If the API experiences an error, [LbryException] is thrown.
   Future<Map> call(method, {Map<String, dynamic> params = const {},
       int timeout = -1}) async {
     timeout = timeout > -1 ? this.timeout : timeout;
