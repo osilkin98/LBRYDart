06a9c0377c58bd675bda69d8ae7902d005e4c068
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 17:48:31 -0600

    Reformats code to comply with Dart standard

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,13 +1,9 @@
-
-
 class LbryException implements Exception {
-
   final num statusCode;
   final String message;
   final Map response;
 
-  LbryException(this.response) :
-        statusCode = response["error"]["code"],
+  LbryException(this.response)
+      : statusCode = response["error"]["code"],
         message = response["error"]["message"];
-
 }
