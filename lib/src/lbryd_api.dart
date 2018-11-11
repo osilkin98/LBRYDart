c075c7cd38394e986a25d9989c6cacc5b235f24b
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 06:52:03 -0600

    Implements call method for the LbrydApi

--- lib/src/lbryd_api.dart
+++ lib/src/lbryd_api.dart
@@ -1,6 +1,21 @@
+import 'dart:async';
+import 'dart:convert';
 import 'base_api.dart';
 
 
 class LbrydApi extends LbryBaseApi {
+  final int timeout;
+  static final String _lbrydUrl = "http://localhost:5279";
+
+  LbrydApi(this.timeout);
+
+  Future<Map> call(String method,
+      {Map<String, dynamic> params = const {}, int timeout = -1}) async {
+    
+    timeout = timeout >= 0 ? this.timeout : timeout;
+    return LbryBaseApi.makeRequest(_lbrydUrl, method,
+        params: params, timeout: timeout);
+    
+  }
 
 }
