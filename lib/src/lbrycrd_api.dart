3963206764801870c3ea07635840ace2c2e1e6be
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 07:27:33 -0600

    Adds the Lbrycrd api file to the private implementations directory

---
+++ lib/src/lbrycrd_api.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'base_api.dart';
+
+
+class LbrycrdApi extends LbryBaseApi {
+  final int timeout;
+  static const String url = "http://localhost:9245";
+  List<String> _basicAuth;
+
+  LbrycrdApi(this._basicAuth,  {this.timeout = 600});
+
+  LbrycrdApi.credentials(String username, String password,
+      {this.timeout = 600}) : _basicAuth = [username, password];
+
+  Future<Map> call(method, {Map<String, dynamic> params = const {},
+      int timeout = -1}) async {
+    timeout = timeout > -1 ? this.timeout : timeout;
+
+  }
+}
