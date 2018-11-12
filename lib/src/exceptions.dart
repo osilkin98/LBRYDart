2a390608152eeab9be168844b37f9d5fba429490
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 19:18:32 -0600

    Adds data field from lbry error to be thrown with exception

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -2,6 +2,7 @@ class LbryException implements Exception {
   final num statusCode;
   final String message;
   final Map response;
+  final List data;
 
   LbryException(this.response)
       : statusCode = response["error"]["code"],
