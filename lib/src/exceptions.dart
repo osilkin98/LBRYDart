c3a16141d2cfc477bf010289d469d17c4274106d
Oleg Silkin <o.silkin98@gmail.com>
2018-11-11 19:19:32 -0600

    Sets the correct fields if `response.error` contains them

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,10 +1,21 @@
 class LbryException implements Exception {
-  final num statusCode;
+  final int statusCode;
   final String message;
   final Map response;
   final List data;
 
-  LbryException(this.response)
-      : statusCode = response["error"]["code"],
-        message = response["error"]["message"];
+  /** Creates a LBRYException object
+   *
+   * Given the Map [response] from the Lbrynet server response,
+   * the [statusCode] int, [message] String, and [data] List are
+   * all obtained from the `error` field in [response].
+   */
+  LbryException(this.response) :
+        statusCode = response["error"].containsKey("code") ?
+          response["error"]["code"] : 0,
+        message = response["error"].containsKey("message") ?
+          response["error"]["message"] : "",
+        data = response["error"].containsKey("data") ?
+          response["error"]["data"] : const [];
+
 }
